a75540942b0d3dd4c02b93ec7638b97d5df1219f
Rahul Chaudhuri <mailrahulsre@gmail.com>
2020-09-07 06:21:46 +0000

    new change

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 v_pass=`cat password`
 echo "connecting to db..."
-mysql -h dbhost.flipkart.com -u dbuser -p $v_pass dev-flipkart <<EOF 
+mysql -h dbhost1.flipkart.com -u dbuser -p $v_pass dev-flipkart <<EOF 
 select * from hr.employee;
 EOF
